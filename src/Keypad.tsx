c13ea608279b96d62d6c682330b1c24aae4ed18c
Non Kittilakkananon <tata32000@hotmail.com>
2024-05-12 22:10:17 +0800

    finish the main function

---
+++ src/Keypad.tsx
@@ -0,0 +1,161 @@
+import { useEffect } from "react";
+
+const Keypad = ({
+  hour,
+  minute,
+  second,
+  setHour,
+  setMinute,
+  setSecond,
+  curPos,
+  setCurPos,
+  setTimeSet,
+  setPrevHour,
+  setPrevMinute,
+  setPrevSecond,
+}: {
+  hour: number;
+  minute: number;
+  second: number;
+  setHour: (hour: number) => void;
+  setMinute: (minute: number) => void;
+  setSecond: (second: number) => void;
+  curPos: string;
+  setCurPos: (curPos: string) => void;
+  setTimeSet: (timeSet: boolean) => void;
+  setPrevHour: (prevHour: number) => void;
+  setPrevMinute: (prevMinute: number) => void;
+  setPrevSecond: (prevSecond: number) => void;
+}) => {
+  const handleSetClick = () => {
+    let prevHour = hour;
+    let prevMinute = minute;
+    let prevSecond = second;
+    if (second >= 60) {
+      prevMinute = minute + Math.floor(second / 60);
+      setMinute(prevMinute);
+      prevSecond = second % 60;
+      setSecond(prevSecond);
+    }
+    if (minute >= 60) {
+      prevHour = hour + Math.floor(minute / 60);
+      setHour(prevHour);
+      prevMinute = minute % 60;
+      setMinute(prevMinute);
+    }
+    setPrevHour(prevHour);
+    setPrevMinute(prevMinute);
+    setPrevSecond(prevSecond);
+    setCurPos("None");
+    setTimeSet(true);
+  };
+  const clearTimer = () => {
+    setHour(0);
+    setMinute(0);
+    setSecond(0);
+    setCurPos("None");
+  };
+  const handleNumberClick = (num: number) => {
+    if (curPos === "hour") {
+      if (hour * 10 + num < 100) {
+        setHour(hour * 10 + num);
+      } else {
+        setHour(num);
+      }
+    } else if (curPos === "minute") {
+      if (minute * 10 + num < 60) {
+        setMinute(minute * 10 + num);
+      } else {
+        setMinute(num);
+      }
+    } else if (curPos === "second") {
+      if (second * 10 + num < 60) {
+        setSecond(second * 10 + num);
+      } else {
+        setSecond(num);
+      }
+    } else {
+      if (hour * 10 + Math.floor(minute / 10) < 100) {
+        setHour(hour * 10 + Math.floor(minute / 10));
+        setMinute((minute % 10) * 10 + Math.floor(second / 10));
+        setSecond((second % 10) * 10 + num);
+      }
+    }
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        setCurPos("None");
+      } else if (e.key >= "0" && e.key <= "9") {
+        handleNumberClick(parseInt(e.key));
+      }
+    };
+
+    const handleClick = (e: MouseEvent) => {
+      if (e.target instanceof Element) {
+        if (e.target.id === "hour") {
+          setCurPos("hour");
+        } else if (e.target.id === "minute") {
+          setCurPos("minute");
+        } else if (e.target.id === "second") {
+          setCurPos("second");
+        } else {
+          setCurPos("None");
+        }
+      }
+    };
+    window.addEventListener("keydown", handleKeyDown);
+    window.addEventListener("click", handleClick);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+      window.removeEventListener("click", handleClick);
+    };
+  });
+  return (
+    <>
+      <div className="flex justify-center mt-4">
+        <div className="flex flex-wrap">
+          {[...Array(5).keys()].map((num) => (
+            <button
+              key={num}
+              className="m-1 p-4 bg-gray-200 text-lg md:text-xl lg:text-2xl xl:text-3xl rounded-md"
+              onClick={() => handleNumberClick(num)}
+            >
+              {num}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-center mt-2">
+        <div className="flex flex-wrap">
+          {[...Array(5).keys()].map((num) => (
+            <button
+              key={num + 5}
+              className="m-1 p-4 bg-gray-200 text-lg md:text-xl lg:text-2xl xl:text-3xl rounded-md"
+              onClick={() => handleNumberClick(num + 5)}
+            >
+              {num + 5}
+            </button>
+          ))}
+        </div>
+      </div>
+      <div className="flex justify-center mt-2">
+        <button
+          className="mx-1 p-4 md:p-6 lg:p-6 xl:p-6 bg-blue-500 text-white text-xl md:text-2xl lg:text-3xl xl:text-4xl rounded-md"
+          onClick={handleSetClick}
+        >
+          Set
+        </button>
+        <button
+          className="mx-1 p-4 md:p-6 lg:p-6 xl:p-6 bg-red-500 text-white text-xl md:text-2xl lg:text-3xl xl:text-4xl rounded-md"
+          onClick={clearTimer}
+        >
+          Clear
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Keypad;
--- src/Timer.tsx
+++ src/Timer.tsx
@@ -1,27 +1,140 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Keypad from "./Keypad";
 
-function pad(num: number, size: number): string {
+const pad = (num: number, size: number): string => {
   let s = num + "";
   while (s.length < size) s = "0" + s;
   return s;
-}
+};
 
 const Timer = () => {
   const [hour, setHour] = useState(0);
   const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
+  const [curPos, setCurPos] = useState("None");
+  const [timeSet, setTimeSet] = useState(false);
+  const [prevHour, setPrevHour] = useState(0);
+  const [prevMinute, setPrevMinute] = useState(0);
+  const [prevSecond, setPrevSecond] = useState(0);
+
+  const buttonStyle = (position: string) => {
+    return `text-4xl md:text-6xl lg:text-8xl xl:text-9xl bg-transparent border-none outline-none cursor-pointer ${
+      curPos === position ? "text-blue-500" : "text-black-600"
+    }`;
+  };
+
+  const handleStartStop = () => {
+    if (isRunning) {
+      setIsRunning(false);
+      return;
+    }
+    setIsRunning(true);
+  };
+
+  const handleClear = () => {
+    if (isRunning) {
+      return;
+    }
+    setHour(prevHour);
+    setMinute(prevMinute);
+    setSecond(prevSecond);
+  };
+
+  useEffect(() => {
+    let timer: ReturnType<typeof setTimeout>;
+    if (isRunning) {
+      timer = setInterval(() => {
+        if (second > 0) {
+          setSecond(second - 1);
+        } else if (minute > 0) {
+          setSecond(59);
+          setMinute(minute - 1);
+        } else if (hour > 0) {
+          setSecond(59);
+          setMinute(59);
+          setHour(hour - 1);
+        } else {
+          setIsRunning(false);
+        }
+      }, 1000);
+    }
+    return () => clearInterval(timer);
+  }, [isRunning, hour, minute, second]);
 
   return (
     <div className="flex items-center justify-center h-screen">
       <div>
-        <h1 className="text-center text-4xl md:text-6xl lg:text-8xl xl:text-9xl">
+        <button
+          className="block mx-auto text-center text-4xl md:text-6xl lg:text-8xl xl:text-9xl bg-transparent border-none outline-none cursor-pointer"
+          onClick={() => setTimeSet(false)}
+        >
           Timer
-        </h1>
-        <div className="flex items-center justify-center box-border border-2 p-4 border-black rounded-lg">
-          <p className="text-4xl md:text-6xl lg:text-8xl xl:text-9xl">
-            {pad(hour, 2)}:{pad(minute, 2)}:{pad(second, 2)}
-          </p>
+        </button>
+        <div className="flex items-center justify-center box-border border-2 p-4 border-black rounded-lg bg-slate-100">
+          <div className="flex">
+            <button
+              className={buttonStyle("hour")}
+              id="hour"
+              onClick={() => setCurPos("hour")}
+            >
+              {pad(hour, 2)}
+            </button>
+            <span className="text-4xl md:text-6xl lg:text-8xl xl:text-9xl mx-1">
+              :
+            </span>
+            <button
+              className={buttonStyle("minute")}
+              id="minute"
+              onClick={() => setCurPos("minute")}
+            >
+              {pad(minute, 2)}
+            </button>
+            <span className="text-4xl md:text-6xl lg:text-8xl xl:text-9xl mx-1">
+              :
+            </span>
+            <button
+              className={buttonStyle("second")}
+              id="second"
+              onClick={() => setCurPos("second")}
+            >
+              {pad(second, 2)}
+            </button>
+          </div>
         </div>
+        {!timeSet ? (
+          <Keypad
+            hour={hour}
+            minute={minute}
+            second={second}
+            setHour={setHour}
+            setMinute={setMinute}
+            setSecond={setSecond}
+            curPos={curPos}
+            setCurPos={setCurPos}
+            setTimeSet={setTimeSet}
+            setPrevHour={setPrevHour}
+            setPrevMinute={setPrevMinute}
+            setPrevSecond={setPrevSecond}
+          />
+        ) : (
+          <div className="flex justify-center mt-4">
+            <button
+              className={`p-4 md:p-6 lg:p-6 xl:p-6 ${
+                isRunning ? "bg-blue-500" : "bg-green-500"
+              } text-white text-xl md:text-2xl lg:text-3xl xl:text-4xl rounded-md mx-1`}
+              onClick={handleStartStop}
+            >
+              {isRunning ? "Stop" : "Start"}
+            </button>
+            <button
+              className="p-4 md:p-6 lg:p-6 xl:p-6 bg-red-500 text-white text-xl md:text-2xl lg:text-3xl xl:text-4xl rounded-md mx-1"
+              onClick={handleClear}
+            >
+              Clear
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
